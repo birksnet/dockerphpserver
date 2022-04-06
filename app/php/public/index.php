97b499f75225cf40716dbc5c54386c46837a1857
Felipe <birksnet@gmail.com>
2022-04-06 07:53:17 -0300

    Servidor Nginx e PHP docker - primeira versão

---
+++ app/php/public/index.php
@@ -0,0 +1,18 @@
+<?php
+echo"
+<!doctype html> \n
+<html> \n 
+<head> \n 
+    <title> Servidor Nginx e PHP-FPM 7.4 </title> \n
+<head> \n
+<body> \n
+<img src='static/logo.png' style='width:10%;margin-left:45%;margin-top:5%;'/>
+<div style='float:center;margin:20%;margin-top:5%;text-align:center;'> \n
+    <h2> Servidor para conteudo statico e dinamico</h2> \n
+    <p> Servidor em nginx e php-fpm 7.4, configurado para distribuir conteudo statico na pasta <b>LOCAL_SERVER/app/php/public/static/</b> </p> \n
+</div> \n
+
+
+</body> \n
+</html> \n
+";
